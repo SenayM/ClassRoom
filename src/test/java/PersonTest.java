a6b8136b6cfed3a84ca2a37e51ea29dc7cbb2267
SenayM <mailtosenay@gmail.com>
2016-10-11 14:25:18 +0200

    After i finish test for person

--- src/test/java/PersonTest.java
+++ src/test/java/PersonTest.java
@@ -5,12 +5,28 @@ import org.junit.Test;
 import otherclasses.Person;
 
 public class PersonTest {
-
+	Person p=new Person("Tesfa", "Mers", 30, 'M');
 	@Test
-	public void test() {
-Person p=new Person("Senay", "Mersie", 30, 'M');
+	public void testFirstName() {
+p.setFirstName("Senay");
 assertEquals(p.getFirstName().equals("Senay"),true);
-
 	}
-
+	@Test
+	public void testLastName() {
+		p.setLastName("Mersie");
+		assertEquals(p.getLastName().equals("Mersie"),true);
+			}
+	@Test
+	public void testAge() {
+p.setAge(31);
+//assertTrue(p.getAge()==31);
+assertEquals(p.getAge(),31);
+	}
+	@Test
+	public void testGender() {
+	p.setGender('M');
+//assertTrue(p.getGender()=='M');
+assertEquals(p.getGender(),'M');
+	
+	}
 }
